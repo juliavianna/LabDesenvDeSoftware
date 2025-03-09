a0c2d62a95a7f303dea3c11a33757015ca95d9ab
Mariana Henriques <112990361+mmhqs@users.noreply.github.com>
2025-03-09 13:55:00 -0300

    feat: create app menu (#5)
    * fix: switch spring to java project
    
    * feat: create classes
    
    * fix: delete spring project
    
    * feat: adding methods to classes
    
    * feat: create app menu

--- implementacao/projetoJava/src/Aluno.java
+++ implementacao/projetoJava/src/Aluno.java
@@ -2,10 +2,16 @@ public class Aluno extends Usuario {
     
     final int MAX_OBRIGATORIAS = 4;
     final int MAX_OPTATIVAS = 2;
-    private int matricula;
     private int periodo;
-    private Disciplina[] disciplinasOptativas;
     private Disciplina[] disciplinasObrigatorias;
+    private Disciplina[] disciplinasOptativas;
+
+    public Aluno(String nome, String email, String senha, int periodo) {
+        super(nome, email, senha);
+        this.periodo = periodo;
+        this.disciplinasObrigatorias = disciplinasObrigatorias;
+        this.disciplinasOptativas = disciplinasOptativas;
+    }
 
     public void matricular(Disciplina disciplina){ 
     }
