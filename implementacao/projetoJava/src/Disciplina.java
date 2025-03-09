f5e27637370c643064eb020c6e98c0ec9554bc16
Mariana Henriques <112990361+mmhqs@users.noreply.github.com>
2025-03-09 13:00:09 -0300

    feat: adding methods (#4)
    * fix: switch spring to java project
    
    * feat: create classes
    
    * fix: delete spring project
    
    * feat: adding methods to classes

--- implementacao/projetoJava/src/Disciplina.java
+++ implementacao/projetoJava/src/Disciplina.java
@@ -1,15 +1,55 @@
 public class Disciplina {
 
+    final int MIN_ALUNOS = 3;
+    final int MAX_ALUNOS = 60;
     private int nome;
     private int periodo;
-    private int vagas;
     private Aluno[] alunos;
+    private boolean disciplinaAtiva;
 
+    public Aluno[] getAlunos() {
+        return alunos;
+    }
+    
     public boolean verificarLimiteVagas(){
         return false;
     }
 
     public void matricularAluno(Aluno aluno){
+        int qtdAlunos = alunos.length;
+        if (qtdAlunos < MAX_ALUNOS) {
+            // Verifica se o aluno já está matriculado (se necessário)
+            for (Aluno a : alunos) {
+                if (a != null && a.equals(aluno)) {
+                    System.out.println("O aluno já está matriculado nesta disciplina.");
+                    return;
+                }
+            }
+
+            // Se a disciplina tiver espaço, matricula o aluno
+            for (int i = 0; i < alunos.length; i++) {
+                if (alunos[i] == null) {
+                    alunos[i] = aluno; // Matrícula o aluno no índice vazio
+                    System.out.println("Aluno matriculado com sucesso!");
+                    return;
+                }
+                
+            }
+        } else {
+            System.out.println("Esta disciplina já está cheia.");
+        }
     }
+
+    public boolean verificarStatus() {
+        int qtdAlunos = alunos.length;
+        if (qtdAlunos < MIN_ALUNOS) {
+            disciplinaAtiva = false;
+        }
+        else {
+            disciplinaAtiva = true;
+        }
+        return disciplinaAtiva;
+    }
+
     
 }
--- implementacao/projetoJava/src/Usuario.java
+++ implementacao/projetoJava/src/Usuario.java
@@ -1,8 +1,49 @@
-public class Usuario {
+import java.util.Random;
+
+public abstract class Usuario {
 
     private int id;
     private String nome;
     private String email;
     private String senha;
     
+        public int getId() {
+            return id;
+        }
+    
+        public void setId() {
+            Random random = new Random();
+            this.id = random.nextInt(10000);
+        }
+    
+        public String getNome() {
+            return nome;
+        }
+    
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+    
+        public String getEmail() {
+            return email;
+        }
+    
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    
+        public void setSenha(String senha) {
+            this.senha = senha;
+        }
+
+        public boolean realizarLogin(String email, String senha) {
+            // Verifica se o email e a senha fornecidos correspondem aos atributos da classe
+            if (this.email.equals(email) && this.senha.equals(senha)) {
+                System.out.println("Login realizado com sucesso!");
+                return true;
+            } else {
+                System.out.println("Email ou senha incorretos.");
+                return false;
+            }
+        }
 }
