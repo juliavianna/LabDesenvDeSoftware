8e8644e700b1736b9572a8b32c008a7bfc995e15
Julia Vianna <julia.vvianna@gmail.com>
2025-04-06 13:17:14 -0300

    feat: selecionar tipo de usuário

--- laboratorio2/implementacao/projetoLab/src/main/java/LabDevSoftware/projetoLab/view/agente_view/src/App.js
+++ laboratorio2/implementacao/projetoLab/src/main/java/LabDevSoftware/projetoLab/view/agente_view/src/App.js
@@ -9,7 +9,8 @@ function App() {
   const [agente, setAgente] = useState(null);
   const [cliente, setCliente] = useState(null);
   const [error, setError] = useState("");
-  const [isEditOpen, setIsEditOpen] = useState(false); // Estado para controlar o colapso
+  const [isEditOpen, setIsEditOpen] = useState(false); 
+  const [tipoUsuario, setTipoUsuario] = useState("");
 
 
   // Buscar todos os agentes ao carregar o componente
@@ -32,43 +33,50 @@ function App() {
     }
   };
 
+  const handleTipoUsuarioChange = (e) => {
+    if (tipoUsuario === e.target.value) {
+      setTipoUsuario("");
+    } else {
+      setTipoUsuario(e.target.value); 
+    }
+  };
 
   const toggleEdit = () => {
     setIsEditOpen(!isEditOpen);
   };
 
   // Atualizar Agenteeee
-const atualizarAgente = async (e) => {
-  e.preventDefault();
-  const id = e.target.id.value;
-  const nome = e.target.nome.value;
-  const senha = e.target.senha.value;
+  const atualizarAgente = async (e) => {
+    e.preventDefault();
+    const id = e.target.id.value;
+    const nome = e.target.nome.value;
+    const senha = e.target.senha.value;
 
-  const agenteAtualizado = { nome, senha };
+    const agenteAtualizado = { nome, senha };
 
-  try {
-    const data = await updateAgente(id, agenteAtualizado);
-    const novosAgentes = agentes.map((agente) =>
-      agente.id === id ? data : agente
-    );
-    setAgentes(novosAgentes);
-    alert("Agente atualizado com sucesso!");
-  } catch (err) {
-    console.error("Erro ao atualizar agente:", err);
-  }
-};
+    try {
+      const data = await updateAgente(id, agenteAtualizado);
+      const novosAgentes = agentes.map((agente) =>
+        agente.id === id ? data : agente
+      );
+      setAgentes(novosAgentes);
+      alert("Agente atualizado com sucesso!");
+    } catch (err) {
+      console.error("Erro ao atualizar agente:", err);
+    }
+  };
 
-// Deletar Agente
-const deletarAgente = async (id) => {
-  try {
-    await deleteAgente(id);
-    const novosAgentes = agentes.filter((agente) => agente.id !== id);
-    setAgentes(novosAgentes);
-    alert("Agente deletado com sucesso!");
-  } catch (err) {
-    console.error("Erro ao deletar agente:", err);
-  }
-};
+  // Deletar Agente
+  const deletarAgente = async (id) => {
+    try {
+      await deleteAgente(id);
+      const novosAgentes = agentes.filter((agente) => agente.id !== id);
+      setAgentes(novosAgentes);
+      alert("Agente deletado com sucesso!");
+    } catch (err) {
+      console.error("Erro ao deletar agente:", err);
+    }
+  };
 
 
   // Criar novo agente
@@ -104,6 +112,7 @@ const deletarAgente = async (id) => {
     e.preventDefault();
     const nome = e.target.nome.value;
     const senha = e.target.senha.value;
+    const email = e.target.email.value;
     const rg = e.target.rg.value;
     const cpf = e.target.cpf.value;
     const bairro = e.target.bairro.value;
@@ -115,7 +124,7 @@ const deletarAgente = async (id) => {
 
     const endereco = { rua, bairro, numero, complemento, cidade, estado };
 
-    const novoCliente = { nome, senha, rg, cpf, endereco };
+    const novoCliente = { nome, email, senha, rg, cpf, endereco };
 
     try {
       const data = await createCliente(novoCliente);
@@ -134,7 +143,67 @@ const deletarAgente = async (id) => {
         <h2>Laboratório de Desenvolvimento de Software</h2>
 
         <h3>Cadastrar Usuário</h3>
-        <form onSubmit={cadastrarAgente}>
+        <p>Qual Tipo de Usuário é você:</p>
+        <label>
+          <input type="radio" name="tipoUsuario" value="cliente" checked={tipoUsuario === "cliente"} onChange={handleTipoUsuarioChange}/>
+          Cliente
+        </label> <br />
+        <label>
+          <input type="radio" name="tipoUsuario" value="empresa_banco" checked={tipoUsuario === "empresa_banco"} onChange={handleTipoUsuarioChange}/>
+          Empresa/Banco
+        </label> <br />
+        {tipoUsuario === "cliente" && (
+          <form onSubmit={cadastrarCliente}>
+            <div style={{ textAlign: 'left' }}>
+              <label htmlFor="nome">Nome: </label>
+              <input type="text" id="nome" name="nome" required placeholder="Digite o nome" />
+            </div>
+            <div style={{ textAlign: 'left' }}>
+              <label htmlFor="email">E-mail: </label>
+              <input type="text" id="email" name="email" required placeholder="Digite o seu e-mail" />
+            </div>
+            <div style={{ textAlign: 'left' }}>
+              <label htmlFor="senha">Senha: </label>
+              <input type="text" id="senha" name="senha" required placeholder="Digite a senha" />
+            </div>
+            <div style={{ textAlign: 'left' }}>
+              <label htmlFor="cpf">CPF: </label>
+              <input type="cpf" id="cpf" name="cpf" required placeholder="Digite o CPF" />
+            </div>
+            <div style={{ textAlign: 'left' }}>
+              <label htmlFor="rg">RG: </label>
+              <input type="rg" id="rg" name="rg" required placeholder="Digite o RG" />
+            </div> 
+            <div style={{ textAlign: 'left' }}>
+              <h4>Endereço</h4>
+              <label htmlFor="bairro" >Bairro: </label>
+              <input type="bairro" id="bairro" name="bairro" required placeholder="Digite o bairro" />
+            </div>
+            <div style={{ textAlign: 'left' }}>
+              <label htmlFor="cidade">Cidade: </label>
+              <input type="cidade" id="cidade" name="cidade" required placeholder="Digite a cidade" />
+            </div>
+            <div style={{ textAlign: 'left' }}>
+              <label htmlFor="complemento">Complemento: </label>
+              <input type="complemento" id="complemento" name="complemento" required placeholder="Digite o complemento" />
+            </div>
+            <div style={{ textAlign: 'left' }}>
+              <label htmlFor="estado">Estado: </label>
+              <input type="estado" id="estado" name="estado" required placeholder="Digite o estado" />
+            </div>
+            <div style={{ textAlign: 'left' }}>
+              <label htmlFor="numero">Número: </label>
+              <input type="numero" id="numero" name="numero" required placeholder="Digite o numero" />
+            </div>
+            <div style={{ textAlign: 'left' }}>
+              <label htmlFor="rua">Rua: </label>
+              <input type="rua" id="rua" name="rua" required placeholder="Digite a rua" />
+            </div>
+            <button type="submit">Cadastrar</button>
+          </form>
+        )}
+        {tipoUsuario === "empresa_banco" && (
+          <form onSubmit={cadastrarAgente}>
           <div style={{ textAlign: 'left' }}>
             <label htmlFor="nome">Nome: </label>
             <input type="text" id="nome" name="nome" required placeholder="Digite o nome" />
@@ -147,24 +216,10 @@ const deletarAgente = async (id) => {
             <label htmlFor="senha">Senha: </label>
             <input type="text" id="senha" name="senha" required placeholder="Digite a senha" />
           </div>
-          {/* <div style={{ textAlign: 'left' }}>
-            <h5>Para Agentes (Bancos e empresas)</h5>
-          </div> */}
           <div style={{ textAlign: 'left' }}>
             <label htmlFor="cnpj">CNPJ: </label>
             <input type="cnpj" id="cnpj" name="cnpj" required placeholder="Digite o CNPJ" />
           </div>
-{/*           <div style={{ textAlign: 'left' }}>
-            <h5>Para Clientes (Pessoa Física)</h5>
-          </div>
-          <div style={{ textAlign: 'left' }}>
-            <label htmlFor="cpf">CPF: </label>
-            <input type="cpf" id="cpf" name="cpf" required placeholder="Digite o CPF" />
-          </div>
-          <div style={{ textAlign: 'left' }}>
-            <label htmlFor="rg">RG: </label>
-            <input type="rg" id="rg" name="rg" required placeholder="Digite o RG" />
-          </div> */}
           <div style={{ textAlign: 'left' }}>
             <h4>Endereço</h4>
             <label htmlFor="bairro" >Bairro: </label>
@@ -192,6 +247,7 @@ const deletarAgente = async (id) => {
           </div>
           <button type="submit">Cadastrar</button>
         </form>
+        )}
 
          <button className="collapsible" onClick={toggleEdit}>
           {isEditOpen ? "Fechar Edição" : "Editar Usuário Existente"}
@@ -265,7 +321,7 @@ const deletarAgente = async (id) => {
         <ul style={{ textAlign: 'left' }}>
           {agentes.map((agente) => (
             <li key={agente.id}>
-              <strong>Nome:</strong> {agente.nome} <br />
+              <strong>Nome:</strong> {agente.nome}
             </li>
           ))}
         </ul>
